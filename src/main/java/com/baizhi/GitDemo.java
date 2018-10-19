664454180f7ec19be046a752030f1525b9d919c3
pl <1078346564@qq.com>
2018-10-19 21:01:03 +0800

    Initial commit

---
+++ src/main/java/com/baizhi/GitDemo.java
@@ -0,0 +1,10 @@
+package com.baizhi;
+
+/**
+ * Created by Administrator on 2018/10/19 0019.
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("第一次提交");
+    }
+}
